7ac073e24db20578cf24bf8c0e8fa172f7281ae3
ro.ershov <ro.ershov@tensor.ru>
2019-10-05 19:19:56 +0300

    add ability to move for 1 player

--- src/Board/index.js
+++ src/Board/index.js
@@ -4,6 +4,7 @@ import './Board.css'
 
 const SQUARE_SIZE = 34;
 
+/*
 const Board = ({size = 2, className = ''}) => 
    <div style={ {width: `${SQUARE_SIZE * size + 'px'}` } } className={'board ' + className}>
       {
@@ -12,6 +13,39 @@ const Board = ({size = 2, className = ''}) =>
          )
       }
    </div>
+*/
 
+class Board extends React.Component {
+   constructor(ptops) {
+      super(ptops);      
+      this.state = {
+         squares: Array(this.props.size ** 2).fill(null)
+      };
+      this.handleClick = this.handleClick.bind(this);
+   }
+   
+   handleClick(squareNumber) {
+      let nextSquareState =  this.state.squares.slice();
+      
+      nextSquareState[squareNumber] = 'X';
+      this.setState({
+         squares: nextSquareState
+      })
+   }
+   
+   render() {
+      let {size, className} = this.props, {squares} = this.state;
+      
+      return (
+         <div style={ {width: `${SQUARE_SIZE * size + 'px'}` } } className={'board ' + className}>
+            {
+               Array.apply( null, Array(size ** 2) ).map( (item, i) => 
+                  <Square key={i} value={squares[i]} onClick={ () => this.handleClick(i)}/>
+               )
+            }
+         </div>
+      )
+   }
+}
 
 export default Board
