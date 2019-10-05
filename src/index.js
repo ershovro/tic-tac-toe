b972813b647724a272d41ad97c70d505e71eca12
ro.ershov <ro.ershov@tensor.ru>
2019-10-05 16:27:17 +0300

    start commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
+//import Game from './Game'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.React = React;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDom.render(
+   <h1>Ready? Go! </h1>,
+   document.getElementById('root')
+)
